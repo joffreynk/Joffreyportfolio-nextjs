11fbfb085b72aea1d833c2f72846d62f59b39df5
JoffreyNK <joyjoffrey@gmail.com>
2022-12-20 23:46:20 +0200

    add projects to the home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import About from '../components/About';
 import Header from '../components/Header';
 import Introduction from '../components/Introduction';
+import Projects from '../components/Projects';
 import Skills from '../components/Skills';
 import WorkingExperience from '../components/WorkingExperience';
 
@@ -36,6 +37,9 @@ export default function Home() {
         </section>
 
         {/* projects */}
+        <section id='projects' className='snap-start'>
+        <Projects />
+        </section>
 
         {/* Contact Me */}
 
