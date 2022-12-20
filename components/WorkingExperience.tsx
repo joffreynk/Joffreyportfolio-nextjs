12c3ce85e870842033ddca8657a3d2d7b8c0ecd7
JoffreyNK <joyjoffrey@gmail.com>
2022-12-20 21:02:45 +0200

    fix overflow

--- components/WorkingExperience.tsx
+++ components/WorkingExperience.tsx
@@ -8,7 +8,7 @@ const WorkingExperience = (props: Props) => {
     <div 
     className='h-screen relative flex flex-col py-10 overflow-hidden text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
         <h3 className="absolute top-16 uppercase tracking-[15px] md:tracking-[20px] text-gray-500 text-2xl">Experience</h3>
-        <div className='flex w-full space-x-5 overflow-x-scroll pt-40 p-10 snap-x snap-mandatory' >
+        <div className='flex w-full space-x-5  overflow-x-scroll pt-40 py-10 snap-x snap-mandatory' >
             <ExperienceCard />
             <ExperienceCard />
             <ExperienceCard />
