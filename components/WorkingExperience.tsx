ab17cb73e48558415443fc859b2960b1176ae89e
JoffreyNK <joyjoffrey@gmail.com>
2022-12-20 22:54:42 +0200

    refactor experience section

--- components/WorkingExperience.tsx
+++ components/WorkingExperience.tsx
@@ -7,7 +7,7 @@ const WorkingExperience = (props: Props) => {
   return (
     <div 
     className='h-screen relative flex flex-col py-10 overflow-hidden text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
-        <h3 className="absolute top-16 uppercase tracking-[15px] md:tracking-[20px] text-gray-500 text-2xl">Experience</h3>
+        <h3 className="absolute top-20 uppercase tracking-[15px] md:tracking-[20px] text-gray-500 text-4xl">Experience</h3>
         <div className='flex w-full space-x-5  overflow-x-scroll pt-40 py-10 snap-x snap-mandatory' >
             <ExperienceCard />
             <ExperienceCard />
