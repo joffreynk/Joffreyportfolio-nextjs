3dcf0d2072bc91ffd74ce9b870c0a7b38d5a8838
JoffreyNK <joyjoffrey@gmail.com>
2022-12-20 20:35:40 +0200

    fix experience cards

--- components/About.tsx
+++ components/About.tsx
@@ -8,60 +8,63 @@ const About = (props: Props) => {
   return (
     <div className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl justify-evenly px-10 mx-auto items-center">
 
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">About</h3>
+      <h3 className="absolute top-16 xl:top-20 uppercase tracking-[20px] text-gray-500 text-2xl">About</h3>
     
-    <div className='mt-24 flex text-justify flex-col relative md:flex-row max-w-7xl justify-evenly mx-auto items-center' >
-      
-      <motion.img
+    <div className='mt-24 flex text-justify flex-col md:flex-row max-w-7xl justify-center mx-2 items-center' >
+     
+        <div className=''>
+        
+          <motion.img
 
-      initial ={{
-        x: -150,
-        opacity: 0,
-      }}
+            initial ={{
+              x: -150,
+              opacity: 0,
+            }}
 
-      transition = {{
-        duration: 1.5,
-      }}
+            transition = {{
+              duration: 1.5,
+            }}
 
-      whileInView={{
-        opacity:1,
-        x:0
-      }}
+            whileInView={{
+              opacity:1,
+              x:0
+            }}
 
-      src={"/passport.jpg"}
+              src={"/professionalpicture.jpg"}
 
-        alt="my professional picture"
+              alt="my professional picture"
 
-        className='md:mb-0 xl:mt-60 w-56 h-56 flex-shrink-0 rounded-full object-cover md:rounded-lg md:w-1/2 md:h-4/5 xl:w-1/2  xl:h-1/2'
-      />
+              className='md:mb-0 w-56 h-56 flex-shrink-0 float-right rounded-full object-cover md:rounded-lg md:w-2/3 md:h-2/3 xl:w-3/4  xl:h-3/4'
+          />
+        </div>
 
-      <motion.div
-      initial ={{
-        x: 150,
-        opacity: 0,
-      }}
+          <motion.div
+          initial ={{
+            x: 150,
+            opacity: 0,
+          }}
 
-      transition = {{
-        duration: 1.5,
-      }}
+          transition = {{
+            duration: 1.5,
+          }}
 
-      whileInView={{
-        opacity:1,
-        x:0
-      }}
-      
-      className="space-y-10 px-0 md:px-10">
-        <h5 className='text-2xl mt-3 font-semibold text-center'>
-          Here is a little about me
-        </h5>
-        <p className='mt-0 pt-0'>
-        Hello I&#39;m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don&#36t hestiate to contact me
-        <br />
-        Hello I&#39;m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don&#36t hestiate to contact me
-        <br />
+          whileInView={{
+            opacity:1,
+            x:0
+          }}
+          
+          className="space-y-10 px-0 md:px-10 flex-grow">
+            <h5 className='text-2xl mt-3 font-semibold text-center'>
+              Here is a little about me
+            </h5>
+            <p className='mt-0 pt-0'>
+            Hello I&#39;m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don&#36t hestiate to contact me
+            <br />
+            Hello I&#39;m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don&#36t hestiate to contact me
+            <br />
 
-        </p>
-      </motion.div>
+            </p>
+        </motion.div>
       </div>
 
     </div>
--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -71,7 +71,7 @@ const ExperienceCard = (props: Props) => {
         <ul className='list-disc space-y-1 xl:space-y-4 px-3 ml-5 text-lg'>
           <li>Designation and elaboration of the program</li>
           <li>Taught 1000+ people computer science</li>
-          <li> 7 months of technical support and computer maintenance</li>
+          <li>7 months of technical support and computer maintenance</li>
         </ul>
       
     </article>
